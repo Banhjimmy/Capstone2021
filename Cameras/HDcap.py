91d36a419db3fd9f34ecf65c9c78507ebae7ef67
Banhjimmy <47795939+Banhjimmy@users.noreply.github.com>
2021-04-08 23:40:37 -0700

    Update HDcap.py

--- Cameras/HDcap.py
+++ Cameras/HDcap.py
@@ -1,12 +1,16 @@
+# Program runs with Python2
+
 import cv2
 import imutils
 import time
 
 # Uncomment These next Two Line for Pi Camera
 # camSet = 'nvarguscamerasrc !  video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-# cam = cv2.VideoCapture(camSet)
+# cap = cv2.VideoCapture(camSet)
 
-cap = cv2.VideoCapture(0)
+# Uncomment next line if usb camera in use 
+# cap = cv2.VideoCapture(0)
+
 ret, HDframe = cap.read()
 
 def takePicture():
